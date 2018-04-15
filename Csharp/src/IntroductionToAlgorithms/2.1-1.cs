a0f4e6adc6f9191a51dcbf73e347196f65189c08
Zhiyuan Chen <zc@int.ac.cn>
2018-04-16 08:00:53 +1000

    Add files via upload

---
+++ Csharp/src/IntroductionToAlgorithms/2.1-1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp.src
+{
+    class InsertationSort
+    {
+        public static int[] InsertSort(int[] array)
+        {
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+            return array;
+        }
+        public static int Main()
+        {
+            int[] array = new int[] { 31, 41, 59, 26, 41, 58 };
+            foreach (var item in InsertSort(array)) { Console.WriteLine(item.ToString()); }
+            return 0;
+        }
+    }
+}
